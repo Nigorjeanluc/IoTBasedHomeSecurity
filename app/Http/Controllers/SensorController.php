50cf988ab28a33a31139cbd1f227b6280145e85a
Nigorjeanluc <nigorjeanluc@gmail.com>
2018-04-05 09:52:24 +0200

    CrsrfToken

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -12,14 +12,10 @@ class SensorController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
     
     public function index()
     {
-        return view('userpages.room1');
+        return view('welcome');
     }
 
     /**
